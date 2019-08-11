b15d3b2d4579a133025aeba4420a1301f33872be
Philip Wilson <pwilson@philips-mbp.lan>
2019-08-11 13:39:55 +0100

    Added welcome message

--- colourtest.py
+++ colourtest.py
@@ -4,8 +4,16 @@ from colormath.color_objects import LabColor, XYZColor, sRGBColor, CMYKColor
 from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie2000, delta_e_cie1994, delta_e_cie1976
 
+greeting = """
+this test uses the colormath python library to:
+1. Construct a CMYKColor object, 
+2. Convert it to L*a*b with convert_color
+3. Use the L*a*b* values to construct a new LabColor object
+4. Convert it to CMYKColor and dump the results.  
 
-print("this test uses the colormath python library to construct a CMYKColor object, convert it to L*a*b, use the L*a*b* values to construct a new LabColor object, then convert it to CMYKColor and dump the results.  We should wind up where we started, but we don't.")
+We should wind up where we started, modulo floating-point minutiae, but we don't.
+"""
+print(greeting)
 
 
 c = CMYKColor(0.75, 0.0, 0.75, 0.2)
